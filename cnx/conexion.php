58f0667b4ea7498308a420484e7de2499689c4d1
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2021-12-06 22:33:32 -0400

    Add files via upload

---
+++ cnx/conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $con = mysqli_connect("id18075256_laboratorio","id18075256_laboratorio1","_^(-EBKs90Lu*W(L","localhost");
+
+    // Check connection
+        if(mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }else{
+        //echo 'conectado';
+    }
+?>
